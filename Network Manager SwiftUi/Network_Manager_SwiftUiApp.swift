3704267ee5c8df770fae8ee330c143c67815c537
Hamid Reza Ansari <hamid.22.ansari@gmail.com>
2024-10-17 16:17:08 +0330

    Initial Commit

---
+++ Network Manager SwiftUi/Network_Manager_SwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Network_Manager_SwiftUiApp.swift
+//  Network Manager SwiftUi
+//
+//  Created by Hamid on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Network_Manager_SwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
